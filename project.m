c787b1876acae825a2d676ed73530e145ddd3429
jonesc10 <cjones434343@gmail.com>
2017-10-24 23:16:19 -0400

    Add option to see the full plot of all 4 quadrants. Also add a sample theta line to the single quadrant version.

--- project.m
+++ project.m
@@ -1,27 +1,32 @@
 % github.com/jonesc10/2ellipses-and-beyond
 
 % How many points for each curve:
-POINTS = 200000;
+POINTS = 2000000;
 % Starting radii of 2 concentric  circles:
-A = 7;
+A = 5;
 B = 10;
 % How deep do you want to go?
-ITERATIONS = 100;
+ITERATIONS = 50;
 % Generate theta values in radians
 thetas = linspace(0, pi/2, POINTS)';
 
+% TOGGLE 0 or 1
+plotFullCircle = 1;
+
+line = @(x) 4*x;
+xs = 0:0.1:10;
+
 % Generate initial points
-ellipse1 = circle(A);
-ellipse2 = circle(B);
+curve1 = circle(A);
+curve2 = circle(B);
 E1 = zeros(POINTS,2);
 E2 = zeros(POINTS,2);
 for i = 1:POINTS
-    E1(i,:) = ellipse1(thetas(i));
-    E2(i,:) = ellipse2(thetas(i));
+    E1(i,:) = curve1(thetas(i));
+    E2(i,:) = curve2(thetas(i));
 end
 X1 = E1(:,1); Y1 = E1(:,2); X2 = E2(:,1); Y2 = E2(:,2);
 % Done with initial point generation
-
 
 for i = 1:ITERATIONS
     % Keep track of previous iteration's points:
@@ -30,11 +35,32 @@ for i = 1:ITERATIONS
     clf;
     hold on;
     pbaspect([1 1 1]);
-    plot(pX1,pY1,'r');
-    plot(pX2,pY2,'r');
-    pause;
-    plot(X1,Y1,'b');
-    plot(X2,Y2,'b');
+    
+    if plotFullCircle
+        allpX1 = [pX1;pX1;-pX1;-pX1];
+        allpY1 = [pY1;-pY1;pY1;-pY1];
+        allpX2 = [pX2;pX2;-pX2;-pX2];
+        allpY2 = [pY2;-pY2;pY2;-pY2];
+
+        allX1 = [X1;X1;-X1;-X1];
+        allY1 = [Y1;-Y1;Y1;-Y1];
+        allX2 = [X2;X2;-X2;-X2];
+        allY2 = [Y2;-Y2;Y2;-Y2];
+        
+        plot(allpX1,allpY1,'*r','MarkerSize',0.1);
+        plot(allpX2,allpY2,'*r','MarkerSize',0.1);
+        pause;
+        plot(allX1,allY1,'*b','MarkerSize',0.1);
+        plot(allX2,allY2,'*b','MarkerSize',0.1);
+    else
+        plot(pX1,pY1,'--r','LineWidth',1.5);
+        plot(pX2,pY2,'--r','LineWidth',1.5);
+        plot(xs, line(xs));
+        axis([0 B 0 B]);
+        pause;
+        plot(X1,Y1,'b');
+        plot(X2,Y2,'b');
+    end
     pause;
 end
 
@@ -47,7 +73,7 @@ function [X1,Y1,X2,Y2] = generatePoints(Axys,Bxys)
     newA = zeros(N,2);
     newB = zeros(N,2);
     while curA <= N & curB <= N-1
-        C = (Axys(curA,2)/Axys(curA,1) - Bxys(curB,2)/Bxys(curB,1));
+        C = Axys(curA,2)/Axys(curA,1) - Bxys(curB,2)/Bxys(curB,1);
         C = C * (Axys(curA,2)/Axys(curA,1) - Bxys(curB+1,2)/Bxys(curB+1,1));
         if sign(C) <= 0
             newA(curI,:) = [Axys(curA,1),(Bxys(curB,2)+Bxys(curB+1,2))/2];
